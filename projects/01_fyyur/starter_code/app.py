f97a33b5ba8db8bc0db3c016f34ec92ee2a446b5
lsurvila <liudas.survila@gmail.com>
2020-12-16 22:56:33 +0100

    search venues

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -16,6 +16,8 @@ from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from sqlalchemy import func
+
 from forms import VenueForm, ArtistForm, ShowForm
 
 # ----------------------------------------------------------------------------#
@@ -141,19 +143,25 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for Hop should return "The Musical Hop".
-    # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+    search_term = request.form.get('search_term', '')
+    query = Venue.query.filter(func.lower(Venue.name).contains(func.lower(search_term)))
+    results = query.all()
+    count = query.count()
+    data = []
+    for i in results:
+        upcoming_shows = Show.query.filter_by(venue_id=i.id).filter(
+                Show.start_time > datetime.utcnow()).count()
+        data.append({
+            "id": i.id,
+            "name": i.name,
+            "num_upcoming_shows": upcoming_shows
+        })
     response = {
-        "count": 1,
-        "data": [{
-            "id": 2,
-            "name": "The Dueling Pianos Bar",
-            "num_upcoming_shows": 0,
-        }]
+        "count": count,
+        "data": data
     }
     return render_template('pages/search_venues.html', results=response,
-                           search_term=request.form.get('search_term', ''))
+                           search_term=search_term)
 
 
 @app.route('/venues/<int:venue_id>')
